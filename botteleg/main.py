0269d1f51e8c8e37a5c68c7a7877c2da010791e4
Niffrai <71142091+Niffrai@users.noreply.github.com>
2023-10-01 23:27:15 +0300

    Add files via upload

--- botteleg/main.py
+++ botteleg/main.py
@@ -8,15 +8,13 @@ def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("Купить")
     btn2 = types.KeyboardButton("Корзина")
-    btn3 = types.KeyboardButton("Поддержка")
-    markup.add(btn1, btn2, btn3)
-    bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я тестовый бот для твоей статьи для habr.com".format(message.from_user), reply_markup=markup)
+    markup.add(btn1, btn2)
+    bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я бот по продаже, посмотри мой ассортимент".format(message.from_user), reply_markup=markup)
     
 @bot.message_handler(content_types=['text'])
 def setting_tel(message):
     if(message.text == "Купить"):
         bot.send_message(message.chat.id, text="Привеет.. Спасибо что читаешь статью!)")
     elif(message.text == "Корзина"):
-        bot.send_message(message.chat.id, text=f"Ваш список покупок:\n{thic_nam}")
-
+        bot.send_message(message.chat.id, text=f"Корзина:\n{thic_nam}")
 bot.polling(none_stop=True)
